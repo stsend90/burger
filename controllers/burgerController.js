7d6ea73f9badd3637520d9134da81ab404cdc199
Jay\^.^/ <stsend90@yahoo.com>
2019-12-27 07:49:24 -0500

    please

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-var burger = require("../models/burger");
+var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
   res.redirect("/burgers");
