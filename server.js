b46ee5e38c8ae12c97511e96362c22d51d392f02
Jay\^.^/ <stsend90@yahoo.com>
2019-12-27 08:48:48 -0500

    test2

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 var express = require("express");
-var bodyParse = require("body-parser");
 var PORT = process.env.PORT || 8080;
 var app = express();
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-//
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
