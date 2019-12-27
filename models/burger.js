c0793adfb046f6d63c2d0c4bb2359b0023d8743a
Jay\^.^/ <stsend90@yahoo.com>
2019-12-27 08:05:42 -0500

    still wokring on it

--- models/burger.js
+++ models/burger.js
@@ -11,17 +11,17 @@ var burger = {
         orm.create("burgers", cols, vals, function(res) {
           cb(res);
         });
-      },
+    },
     update: function(objColVals, condition, cb) {
         orm.update("burgers", objColVals, condition, function(res) {
           cb(res);
         });
-      },
+    },
     delete: function(condition, cb) {
         orm.delete("burgers", condition, function(res) {
           cb(res);
         });
-      }
-    };
+    }
+};
 
 module.exports = burger;
