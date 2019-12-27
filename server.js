8283930383ea26b6d03fd7ee73290a94f6f5f49b
Jay\^.^/ <stsend90@yahoo.com>
2019-12-27 06:12:54 -0500

    done

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 app.use(express.static("public"));
 
